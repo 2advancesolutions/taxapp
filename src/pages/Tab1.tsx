52ab19a80aa4844b99a3b2b58050b10f3140fa62
batman <2advancesolutions@gmail.com>
2023-01-29 14:31:52 -0600

    added project setup files ionic

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          hjg
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+       
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
