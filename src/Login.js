3bb229c71d8db3789b8da1edf4026ed8e0d39bd0
batman <2advancesolutions@gmail.com>
2023-01-29 20:02:03 -0600

    login works with pocket db need to clean up login screen

--- src/Login.js
+++ src/Login.js
@@ -1,35 +1,45 @@
 import pb from "lib/pocketbase"
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Auth() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Email:", email);
-        console.log("Password:", password);
+    const { register, handleSubmit } = useForm()
+    const [isLoading, setLoading] = useState(false)
+
+    const login = async (data) => {
+        setLoading(true);
+        try {
+            const authData = await pb.collection("users")
+                .authWithPassword(data.email, data.password);
+            setLoading(false);
+
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+
     };
+
     return (<>
+         {isLoading && <h1>Loading....</h1>}
         <h1>Logged In {pb.authStore.isValid.toString()}</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(login)}>
             <label htmlFor="email">Email:</label>
             <input
                 type="email"
                 id="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                {...register("email")}
             />
             <br />
             <label htmlFor="password">Password:</label>
             <input
                 type="password"
                 id="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
+                {...register("password")}
             />
             <br />
-            <button type="submit">Login</button>
+            <button type="submit" disabled={isLoading}>{isLoading ? "Loading" : "Login"}</button>
         </form>
     </>);
 }
